eec82e75086c1140cfbdd7183e2d916d1c8ef8ee
Aniket Raj <aniketronaldo10@gmail.com>
2020-12-13 02:23:21 +0530

    Login base form setup complete
    Need to add validation and send request to API

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Login from './pages/login/login.page';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-		<Login />
-    </div>
-  );
+	return (
+		<div className="App" id="center">
+			<Login />
+		</div>
+	);
 }
 
 export default App;
--- src/pages/login/login.page.jsx
+++ src/pages/login/login.page.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Frame } from 'framer';
+import { Form, FormGroup, Label, Input, Button, Col } from 'reactstrap';
+import './login.styles.css';
 
 class Login extends Component {
     constructor() {
@@ -9,10 +12,65 @@ class Login extends Component {
             password: ''
         };
     }
-    
+
+    handleInputChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [field]: value
+        });
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(this.state);
+    }
+
     render() {
         return (
-            <p>Gaylo, World!</p>
+            <Frame
+                width={400}
+                height={400}
+                radius={30}
+                animate={{ scale: 1.1 }}
+                transition={{ duration: 2 }}
+                background={"#61DAFB"}
+            >
+                <div className="container">
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormGroup row>
+                            <Col>
+                                <Input type="email" id="email" name="email"
+                                    placeholder="Email"
+                                    value={this.state.email}
+                                    onChange={this.handleInputChange} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Col>
+                                <Input type="password" id="password" name="password"
+                                    placeholder="Password"
+                                    value={this.state.password}
+                                    onChange={this.handleInputChange} />
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                    <div className="row">
+                        <div className="col-12" align="center">
+                            <Frame
+                                width={100}
+                                height={30}
+                                onTap={this.handleSubmit}
+                                whileHover={{ scale: 1.2 }}
+                            >
+                                Login
+                            </Frame>
+                        </div>
+                    </div>
+                </div>
+
+            </Frame>
         );
     }
 }
