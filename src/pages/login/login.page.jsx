71732bcd9426b674fc37f20080335479b6754352
Aniket Raj <aniketronaldo10@gmail.com>
2020-12-19 02:32:00 +0530

    Validation for Email ID and Password added
    Branch ready to merge
    
    TODO:
    - Add a loader

--- src/pages/login/login.page.jsx
+++ src/pages/login/login.page.jsx
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 import './login.styles.css';
 
 toast.configure();
+const validEmailRegex = RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
 
 class Login extends Component {
     constructor() {
@@ -12,7 +13,11 @@ class Login extends Component {
         this.state = {
             username: '',
             password: '',
-            user_auth_token: ''
+            user_auth_token: '',
+            errors: {
+                username: '',
+                password: ''
+            }
         };
     }
 
@@ -26,6 +31,20 @@ class Login extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
+
+        const errors = this.state.errors;
+
+        if (errors.username.length !== 0 || errors.password.length !== 0) {
+            console.log('bahhhas');
+            return;
+        }
+
+        if (this.state.username.length === 0 || this.state.password.length === 0) {
+            toast.error(`Empty Fields 💔`, {
+                position: toast.POSITION.TOP_CENTER
+            });
+            return;
+        }
 
         let loginData = new FormData();
         loginData.append('username', this.state.username);
@@ -68,19 +87,59 @@ class Login extends Component {
             }));
     }
 
+    validateField = e => {
+        e.preventDefault();
+
+        const { name, value } = e.target;
+        const errors = this.state.errors;
+
+        switch (name) {
+            case 'username':
+                errors.username = validEmailRegex.test(value) ? '' : 'Your Email ID does not look right 🤔';
+                break;
+
+            case 'password':
+                errors.password = value.length > 0 ? '' : 'Please enter a valid password 🥺';
+                break;
+
+            default:
+                break;
+        }
+
+        this.setState({ errors });
+    }
+
     render() {
         return (
             <Frame
-                width={400}
-                height={400}
+                width={450}
+                height={450}
                 radius={30}
-                animate={{ scale: 1.1 }}
-                transition={{ duration: 2 }}
+                animate={{ scale: 0.95 }}
+                transition={{ duration: 0.5 }}
                 background={"#CCCCCC"}
             >
                 <div className="flex-container">
-                    <input type="email" name="username" placeholder="Email ID" value={this.state.username} onChange={this.handleInputChange} />
-                    <input type="password" name="password" placeholder={"Password"} value={this.state.password} onChange={this.handleInputChange} />
+                    <input
+                        type="email"
+                        name="username"
+                        placeholder="Email ID"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        onBlur={this.validateField}
+                    />
+                    {this.state.errors.username.length > 0 ? <span className="errors">{this.state.errors.username}</span> : null}
+
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        onBlur={this.validateField}
+                    />
+                    {this.state.errors.username.length > 0 ? <span className="errors">{this.state.errors.password}</span> : null}
+
                     <button className="submit--button" onClick={this.handleSubmit}>Submit</button>
                 </div>
             </Frame>
