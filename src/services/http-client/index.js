dcf653546158359bbdbdc554c85f46d526bdb3dc
Pranav Bakre <psbakre@yahoo.com>
2021-01-18 17:51:13 +0530

    removed unnecessary braces

--- src/services/http-client/index.js
+++ src/services/http-client/index.js
@@ -1,8 +1,6 @@
-const httpClient = async (url, parameters) => {
-    return await fetch(url, parameters)
+const httpClient = async (url, parameters) => await fetch(url, parameters)
         .then(async response => {
             const json = await response.json();
             return response.ok ? json : Promise.reject(json);
         })
-}
 export default httpClient;
