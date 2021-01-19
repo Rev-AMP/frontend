46a990599ce58e10d2816d2575a938bbb4d02277
Akhil Narang <me@akhilnarang.dev>
2021-01-19 21:38:37 +0530

    App.js: remove the remnants of react-cookie to fix build failure
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import Main from './pages/main/main.page';
 import { HashRouter } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
 import './App.css';
 
 function App() {
 	return (
-		<CookiesProvider>
-			<HashRouter>
-				<div className="App" id="center">
-					<Main />
-				</div>
-			</HashRouter>
-		</CookiesProvider>
+		<HashRouter>
+			<div className="App" id="center">
+				<Main />
+			</div>
+		</HashRouter>
 	);
 }
 
