7fae0307e27e576e0fecb5694c12474d947e8624
ksdfg <ksdfg123@gmail.com>
2021-03-12 17:34:04 +0530

    pages/schools: add delete school action to DataPage

--- src/pages/schools/index.jsx
+++ src/pages/schools/index.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { IconButton, Tooltip, withStyles } from "@material-ui/core";
-import { Edit } from "@material-ui/icons";
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    IconButton,
+    Tooltip,
+    Typography,
+    withStyles,
+} from "@material-ui/core";
+import { Delete, Edit } from "@material-ui/icons";
 
-import { FetchSchools } from "redux/school/action";
+import { FetchSchools, DeleteSchool } from "redux/school/action";
 import SchoolModal from "./components/SchoolModal";
 import DataPage from "components/DataPage";
+import Button from "components/Button";
 
 const styles = (theme) => ({
     centerItem: theme.styles.centerItem,
+    error: {
+        color: theme.palette.error.main,
+        "&:hover": {
+            backgroundColor: theme.palette.error.main,
+            color: "#000000",
+        },
+    },
+    success: {
+        color: theme.palette.success.main,
+        "&:hover": {
+            backgroundColor: theme.palette.success.main,
+            color: "#000000",
+        },
+    },
 });
 
 class Schools extends React.Component {
@@ -42,15 +67,18 @@ class Schools extends React.Component {
             flex: 1,
             sortable: false,
             renderCell: (params) => (
-                <Tooltip title="Edit">
-                    <IconButton
-                        className={this.props.classes.centerItem}
-                        color={"primary"}
-                        onClick={() => this.onEdit(params)}
-                    >
-                        <Edit />
-                    </IconButton>
-                </Tooltip>
+                <div className={this.props.classes.centerItem}>
+                    <Tooltip title="Edit">
+                        <IconButton color={"primary"} onClick={() => this.onEdit(params)}>
+                            <Edit />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Delete">
+                        <IconButton onClick={() => this.onDelete(params)}>
+                            <Delete color={"error"} />
+                        </IconButton>
+                    </Tooltip>
+                </div>
             ),
         },
     ];
@@ -59,6 +87,7 @@ class Schools extends React.Component {
         super(props);
         this.state = {
             modalIsOpen: false,
+            deleteConfirmAlert: false,
         };
     }
 
@@ -66,35 +95,62 @@ class Schools extends React.Component {
         this.props.FetchSchools();
     }
 
-    closeModal = () => {
-        this.setState({ modalIsOpen: false, schoolId: null });
-    };
+    closeModal = () => this.setState({ modalIsOpen: false, schoolId: null });
 
-    openModal = () => {
-        this.setState({ modalIsOpen: true, schoolId: null });
-    };
+    openModal = () => this.setState({ modalIsOpen: true, schoolId: null });
 
-    onEdit = (params) => {
-        this.setState({ modalIsOpen: true, schoolId: params.row.id });
+    onEdit = (params) => this.setState({ modalIsOpen: true, schoolId: params.row.id });
+
+    onDelete = (params) => this.setState({ deleteConfirmAlert: true, schoolId: params.row.id });
+
+    onDeleteClose = () => this.setState({ deleteConfirmAlert: false, schoolId: null });
+
+    deleteSchool = () => {
+        this.props.DeleteSchool(this.state.schoolId);
+        this.onDeleteClose();
     };
 
     render() {
+        const { isLoading, schools, classes } = this.props;
+        const { modalIsOpen, schoolId, deleteConfirmAlert } = this.state;
+
         return (
-            <DataPage
-                title="List of Schools"
-                isLoading={this.props.isLoading}
-                modalIsOpen={this.state.modalIsOpen}
-                openModal={this.openModal}
-                PopupModal={
-                    <SchoolModal
-                        isOpen={this.state.modalIsOpen}
-                        onClose={this.closeModal}
-                        schoolId={this.state.schoolId}
-                    />
-                }
-                objects={this.props.schools}
-                columns={this.columns}
-            />
+            <>
+                <DataPage
+                    title="List of Schools"
+                    isLoading={isLoading}
+                    modalIsOpen={modalIsOpen}
+                    openModal={this.openModal}
+                    PopupModal={<SchoolModal isOpen={modalIsOpen} onClose={this.closeModal} schoolId={schoolId} />}
+                    objects={schools}
+                    columns={this.columns}
+                />
+                {deleteConfirmAlert && schoolId && (
+                    <Dialog open={deleteConfirmAlert} onClose={this.onDeleteClose}>
+                        <DialogTitle>
+                            <Typography variant="h6" color="primary">
+                                Delete Year?
+                            </Typography>
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Deleting this <strong>School</strong> may lead to unwanted consequences like deletion of
+                                <strong> Users</strong>, <strong>Years</strong>, <strong>Terms</strong> and{" "}
+                                <strong>Courses</strong> belong to this <strong>School</strong>. Are you sure you want
+                                to proceed?
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={this.onDeleteClose} className={classes.error}>
+                                No
+                            </Button>
+                            <Button onClick={this.deleteSchool} className={classes.success}>
+                                yes
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                )}
+            </>
         );
     }
 }
@@ -104,4 +160,4 @@ const mapStateToProps = (state) => ({
     isLoading: state.school.isLoading,
 });
 
-export default withRouter(withStyles(styles)(connect(mapStateToProps, { FetchSchools })(Schools)));
+export default withRouter(withStyles(styles)(connect(mapStateToProps, { FetchSchools, DeleteSchool })(Schools)));
