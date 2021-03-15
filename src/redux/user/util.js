c739ba1c356917478da38fdce02c0e58bb3c2346
Pranav Bakre <psbakre@yahoo.com>
2021-03-15 12:50:00 +0530

    setProfilePicture is a function now and will return a generator so essentially yield setProfilePicture will do yield APICall

--- src/redux/user/util.js
+++ src/redux/user/util.js
@@ -3,11 +3,11 @@ import { APICall } from "../../services/http-client";
 export const addProfilePictureURL = (user) =>
     `${process.env.REACT_APP_BACKEND_URL}/profile_pictures/${user.profile_picture}`;
 
-export function* setProfilePicture(userID, profilePicture) {
+export function setProfilePicture(userID, profilePicture) {
     const formData = new FormData();
     formData.append("image", profilePicture);
 
-    return yield APICall(`/api/v1/users/${userID}/profile_picture`, {
+    return APICall(`/api/v1/users/${userID}/profile_picture`, {
         method: "PUT",
         body: formData,
     });
