9e709f88b5c19847b69243a6d6e9edd9aa2d6ef1
ksdfg <ksdfg123@gmail.com>
2021-03-15 19:54:44 +0530

    utils: formatting

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const getUpdatedInfo = (baseObj, submitObj) => {
     const updatedInfo = {};
-    for (const key in submitObj){
+    for (const key in submitObj) {
         if (baseObj[key] !== submitObj[key]) updatedInfo[key] = submitObj[key];
     }
     return updatedInfo;
