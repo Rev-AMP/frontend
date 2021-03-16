c3869c1b9b6f2824337c17b3db2e3a463f15b786
ksdfg <ksdfg123@gmail.com>
2021-03-16 19:58:53 +0530

    components/AuthenticatedRoute: no need to check the same condition in two subsequent components
    also remove outdated component

--- src/components/AuthenticatedRoute/index.jsx
+++ src/components/AuthenticatedRoute/index.jsx
@@ -7,82 +7,6 @@ import Loader from "components/Loader";
 import { fetchUserMe } from "redux/user/action";
 import { fetchAdminMe } from "redux/admin/action";
 
-class AuthenticatedRoute extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            userFetched: false,
-            adminFetched: false,
-        };
-    }
-
-    checkPerms = () => {
-        const { permission, currentUser, currentAdmin, fetchAdminMe } = this.props;
-        const { adminFetched } = this.state;
-        if (permission && currentUser) {
-            if (currentUser.is_admin) {
-                if (!currentAdmin) {
-                    if (!adminFetched) {
-                        fetchAdminMe();
-                        this.setState({ adminFetched: true });
-                    }
-                } else if (!currentAdmin.permissions.isAllowed(permission)) {
-                    toast.error(`Error 😓: You don't have ${permission} permissions`, {
-                        position: toast.POSITION.TOP_CENTER,
-                    });
-                }
-            } else {
-                toast.error(`Error 😓: You don't have ${permission} permissions`, {
-                    position: toast.POSITION.TOP_CENTER,
-                });
-            }
-        }
-    };
-
-    componentDidMount() {
-        if (this.props.rehydrated) {
-            this.checkPerms();
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        const { rehydrated, isLoggedIn, currentUser, fetchUserMe } = this.props;
-        const { userFetched } = this.state;
-        if (rehydrated) {
-            if (isLoggedIn && !currentUser && !userFetched) {
-                fetchUserMe();
-                this.setState({ userFetched: true });
-            }
-            this.checkPerms();
-        }
-    }
-
-    render() {
-        const { rehydrated, permission, currentAdmin, isLoggedIn, currentUser, isLoading, ...otherProps } = this.props;
-
-        if (
-            !rehydrated ||
-            (isLoggedIn && !currentUser) ||
-            (permission && currentUser && currentUser.is_admin && !currentAdmin)
-        ) {
-            return <Loader />;
-        } else if (isLoggedIn) {
-            if (currentUser) {
-                if (permission) {
-                    if (currentUser.is_admin && currentAdmin && currentAdmin.permissions.isAllowed(permission)) {
-                        return <Route {...otherProps} />;
-                    }
-                    return <Redirect to="/app" />;
-                }
-                return <Route {...otherProps} />;
-            }
-            return <Loader />;
-        }
-        return <Redirect to="/login" />;
-    }
-}
-
 function AuthenticateRoute(props) {
     const {
         rehydrated,
@@ -103,17 +27,18 @@ function AuthenticateRoute(props) {
     }, [rehydrated, selfDataLoading, fetchUserMe]);
 
     useEffect(() => {
-        if (currentUser && currentUser.is_admin && permission && !currentAdmin) {
-            fetchAdminMe();
-        }
-        if (
-            permission &&
-            ((currentUser && !currentUser.is_admin) ||
-                (currentAdmin && !currentAdmin.permissions.isAllowed(permission)))
-        ) {
-            toast.error(`Error 😓: You don't have ${permission} permissions`, {
-                position: toast.POSITION.TOP_CENTER,
-            });
+        if (permission) {
+            if (currentUser && currentUser.is_admin && !currentAdmin) {
+                fetchAdminMe();
+            }
+            if (
+                (currentUser && !currentUser.is_admin) ||
+                (currentAdmin && !currentAdmin.permissions.isAllowed(permission))
+            ) {
+                toast.error(`Error 😓: You don't have ${permission} permissions`, {
+                    position: toast.POSITION.TOP_CENTER,
+                });
+            }
         }
     }, [currentUser, permission, currentAdmin, fetchAdminMe]);
 
