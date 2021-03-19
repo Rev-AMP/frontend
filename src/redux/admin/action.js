c0b3b0bd0534258316a1b90391ea272659ab028f
ksdfg <ksdfg123@gmail.com>
2021-03-19 15:23:26 +0530

    redux/admin: add redux and saga for new and updated admins API endpoints

--- src/redux/admin/action.js
+++ src/redux/admin/action.js
@@ -1,15 +1,51 @@
 import AdminActionTypes from "./action.types";
 
+export const fetchAdmins = () => ({
+    type: AdminActionTypes.FETCH_ADMINS,
+});
+export const fetchAdminsSuccess = (admins) => ({
+    type: AdminActionTypes.FETCH_ADMINS_SUCCESS,
+    payload: admins,
+});
+export const fetchAdminsFailure = (error) => ({
+    type: AdminActionTypes.FETCH_ADMINS_FAILURE,
+    payload: error,
+});
+
+export const updateAdmin = (adminUpdate) => ({
+    type: AdminActionTypes.UPDATE_ADMIN,
+    payload: adminUpdate,
+});
+export const updateAdminSuccess = (admin) => ({
+    type: AdminActionTypes.UPDATE_ADMIN_SUCCESS,
+    payload: admin,
+});
+export const updateAdminFailure = (error) => ({
+    type: AdminActionTypes.UPDATE_ADMIN_FAILURE,
+    payload: error,
+});
+
 export const fetchAdminMe = () => ({
     type: AdminActionTypes.FETCH_ADMIN_ME,
 });
-
 export const fetchAdminMeSuccess = (user) => ({
     type: AdminActionTypes.FETCH_ADMIN_ME_SUCCESS,
     payload: user,
 });
-
 export const fetchAdminMeFailure = (error) => ({
     type: AdminActionTypes.FETCH_ADMIN_ME_FAILURE,
+    payload: error,
+});
+
+export const fetchAdmin = (id) => ({
+    type: AdminActionTypes.FETCH_ADMIN,
+    payload: id,
+});
+export const fetchAdminSuccess = (admin) => ({
+    type: AdminActionTypes.FETCH_ADMIN_SUCCESS,
+    payload: admin,
+});
+export const fetchAdminFailure = (error) => ({
+    type: AdminActionTypes.FETCH_ADMIN_FAILURE,
     payload: error,
 });
--- src/redux/admin/saga.js
+++ src/redux/admin/saga.js
@@ -1,9 +1,50 @@
-import { all, put, takeEvery } from "redux-saga/effects";
+import { all, put, select, takeEvery } from "redux-saga/effects";
 
 import AdminActionTypes from "./action.types";
-import { fetchAdminMeFailure, fetchAdminMeSuccess } from "./action";
+import {
+    fetchAdminMeFailure,
+    fetchAdminMeSuccess,
+    fetchAdminsSuccess,
+    fetchAdminsFailure,
+    updateAdminSuccess,
+    updateAdminFailure,
+    fetchAdminSuccess,
+    fetchAdminFailure,
+    fetchAdmins as actionFetchAdmins,
+} from "./action";
 import { APICall } from "services/http-client";
 import { AdminPermissions } from "services/admin";
+
+function* fetchAdmins() {
+    yield takeEvery(AdminActionTypes.FETCH_ADMINS, function* () {
+        try {
+            const admins = yield APICall(`/api/v1/admins/`, {
+                method: "GET",
+            });
+            admins.forEach((admin, index) => (admins[index].permissions = new AdminPermissions(admin.permissions)));
+            yield put(fetchAdminsSuccess(admins));
+        } catch (error) {
+            yield put(fetchAdminsFailure(error.detail));
+        }
+    });
+}
+
+function* updateAdmin() {
+    yield takeEvery(AdminActionTypes.UPDATE_ADMIN, function* (action) {
+        try {
+            const selectedAdmin = yield select((state) => state.admin.selectedAdmin);
+            const admin = yield APICall(`/api/v1/admins/${selectedAdmin.id}`, {
+                method: "PUT",
+                body: JSON.stringify(action.payload),
+            });
+            admin.permissions = new AdminPermissions(admin.permissions);
+            yield put(updateAdminSuccess(admin));
+            yield put(actionFetchAdmins()); // update list of all admins
+        } catch (error) {
+            yield put(updateAdminFailure(error.detail));
+        }
+    });
+}
 
 function* fetchAdminMe() {
     yield takeEvery(AdminActionTypes.FETCH_ADMIN_ME, function* () {
@@ -17,8 +58,20 @@ function* fetchAdminMe() {
     });
 }
 
+function* fetchAdmin() {
+    yield takeEvery(AdminActionTypes.FETCH_ADMIN, function* (action) {
+        try {
+            let admin = yield APICall(`/api/v1/admins/${action.payload}`, { method: "GET" });
+            admin.permissions = new AdminPermissions(admin.permissions);
+            yield put(fetchAdminSuccess(admin));
+        } catch (error) {
+            yield put(fetchAdminFailure(error.detail));
+        }
+    });
+}
+
 function* fetchAdminMethods() {
-    yield all([fetchAdminMe()]);
+    yield all([fetchAdmins(), updateAdmin(), fetchAdmin(), fetchAdminMe()]);
 }
 
 export default fetchAdminMethods;
