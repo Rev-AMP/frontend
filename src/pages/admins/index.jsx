1853262f89d23d46357e72688dffda3ea66b7f6e
ksdfg <ksdfg123@gmail.com>
2021-04-11 22:57:58 +0530

    pages: adjust ID fields for UUID

--- src/pages/admins/index.jsx
+++ src/pages/admins/index.jsx
@@ -22,8 +22,9 @@ class Admins extends React.Component {
             headerName: "ID",
             headerAlign: "center",
             align: "center",
-            width: 70,
+            width: 350,
             type: "number",
+            hide: true,
         },
         {
             field: "full_name",
