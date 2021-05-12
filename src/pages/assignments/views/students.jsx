71ab21047cf3b9a3a33a8433bcf7b6f527dd027b
ksdfg <ksdfg123@gmail.com>
2021-05-12 16:08:26 +0530

    pages: assignments: students: use actions icon button to upload assignments
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- src/pages/assignments/views/students.jsx
+++ src/pages/assignments/views/students.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import clsx from "clsx";
-import { Check, Delete } from "mdi-material-ui";
-import { withStyles, Tooltip, IconButton } from "@material-ui/core";
+import { Check, Close, Delete, FileUpload } from "mdi-material-ui";
+import { IconButton, Tooltip, withStyles } from "@material-ui/core";
 
 import DataPage from "components/DataPage";
-import Button from "components/Button";
 
 let ASSIGNMENTS = [
     {
@@ -69,7 +68,7 @@ class StudentAssignments extends React.Component {
             headerName: "ID",
             headerAlign: "center",
             align: "center",
-            width: 50,
+            width: 70,
             hide: true,
         },
         {
@@ -77,44 +76,33 @@ class StudentAssignments extends React.Component {
             headerName: "Name",
             headerAlign: "center",
             align: "center",
-            width: 250,
+            width: 350,
         },
         {
             field: "professor",
             headerName: "Professor",
             headerAlign: "center",
             align: "center",
-            width: 250,
+            width: 350,
         },
         {
             field: "subject",
-            headerName: "Subject",
-            headerAlign: "center",
-            align: "center",
-            width: 250,
-        },
-        {
-            field: "submitted",
-            headerName: "Submission Status",
+            headerName: "Course",
             headerAlign: "center",
             align: "center",
             width: 350,
+        },
+        {
+            field: "submitted",
+            headerName: "Status",
+            headerAlign: "center",
+            align: "center",
+            flex: 1,
             renderCell: (params) =>
                 params.value ? (
                     <Check className={clsx(this.props.classes.centerItem, this.props.classes.green)} />
                 ) : (
-                    <div className={this.props.classes.centerItem}>
-                        <input
-                            type="file"
-                            name="file"
-                            style={{ display: "none" }}
-                            ref={(fileInput) => (this.fileInput = fileInput)}
-                            onChange={() => this.fileUpload(params.row.id)}
-                        />
-                        <Button onClick={() => this.fileInput.click()} variant="contained">
-                            Upload
-                        </Button>
-                    </div>
+                    <Close color="error" className={this.props.classes.centerItem} />
                 ),
         },
         {
@@ -122,26 +110,41 @@ class StudentAssignments extends React.Component {
             headerName: "Grade",
             headerAlign: "center",
             align: "center",
-            width: 100,
-            renderCell: (params) =>
-                params.value ? (
-                    <p className={this.props.classes.centerItem}>{params.value}</p>
-                ) : (
-                    <p className={this.props.classes.centerItem}>-</p>
-                ),
+            flex: 1,
+            valueFormatter: (params) => (params.value ? params.value : "-"),
         },
         {
             field: "actions",
             headerName: "Actions",
             headerAlign: "center",
             align: "center",
-            width: 100,
+            flex: 1,
             renderCell: (params) =>
-            <Tooltip title="Delete">
-                <IconButton onClick={() => this.onDelete(params.row.id)}>
-                    <Delete color={"error"} />
-                </IconButton>
-            </Tooltip>
+                params.row.submitted ? (
+                    <Tooltip title="Delete">
+                        <IconButton
+                            className={this.props.classes.centerItem}
+                            onClick={() => this.onDelete(params.row.id)}
+                        >
+                            <Delete color={"error"} />
+                        </IconButton>
+                    </Tooltip>
+                ) : (
+                    <Tooltip title="Upload">
+                        <div className={this.props.classes.centerItem}>
+                            <input
+                                type="file"
+                                name="file"
+                                style={{ display: "none" }}
+                                ref={(fileInput) => (this.fileInput = fileInput)}
+                                onChange={() => this.fileUpload(params.row.id)}
+                            />
+                            <IconButton onClick={() => this.fileInput.click()}>
+                                <FileUpload color={"primary"} />
+                            </IconButton>
+                        </div>
+                    </Tooltip>
+                ),
         },
     ];
 
@@ -157,7 +160,7 @@ class StudentAssignments extends React.Component {
         assignments[id].submitted = false;
 
         this.setState(assignments);
-    }
+    };
 
     render() {
         const { assignments, isLoading } = this.state;
