5c2bc2346aeac004dd8b039f5f08235301b1a4f9
Aniket Raj <aniketronaldo10@gmail.com>
2021-05-12 16:42:52 +0530

    Professor Assignments Table

--- src/pages/assignments/index.jsx
+++ src/pages/assignments/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { connect } from 'react-redux';
 import StudentAssignments from "./views/students";
+import ProfessorAssignmentsDetails from "./views/assignments";
 
 function Assignments({ currentUser }) {
-    return (currentUser.type === "student" ? <StudentAssignments /> : <p>Error</p>);
+    switch (currentUser.type) {
+        case "student": return <StudentAssignments />;
+        case "professor": return <ProfessorAssignmentsDetails /> ;
+        default: return <p>Unknown</p>;
+    }
 }
 
 const mapStateToProps = (state) => ({
