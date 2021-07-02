ac28f06c103c7e063afc5aee93ac2c19f5a9d2c8
ksdfg <ksdfg123@gmail.com>
2021-07-03 01:13:57 +0530

    terms: display school name in select component
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- src/pages/terms/components/TermSelect.jsx
+++ src/pages/terms/components/TermSelect.jsx
@@ -5,13 +5,31 @@ import { fetchTerms } from "redux/term/action";
 import DataSelect from "components/DataSelect";
 
 class TermSelect extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            meow: [],
+        };
+    }
+
     componentDidMount() {
         this.props.fetchTerms();
     }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.terms !== prevProps.terms && this.props.terms) {
+            let meow = this.state.meow;
+            this.props.terms.forEach((term, index) => {
+                term.name += ` : ${term.year.school.name}`;
+                meow.push(term);
+            });
+            this.setState({ meow });
+        }
+    }
+
     render() {
         const { isLoading, terms, ...otherProps } = this.props;
-        return <DataSelect isLoading={isLoading} data={terms} {...otherProps} />;
+        return <DataSelect isLoading={isLoading} data={this.state.meow} {...otherProps} />;
     }
 }
 
