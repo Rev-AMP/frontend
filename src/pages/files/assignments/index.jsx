32f250c7832578d0c7d0dd963d64227fe32f5e64
ksdfg <ksdfg123@gmail.com>
2021-07-03 00:52:56 +0530

    files: add download links
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- src/pages/files/assignments/index.jsx
+++ src/pages/files/assignments/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { withStyles } from "@material-ui/core";
+import { IconButton, Tooltip, withStyles } from "@material-ui/core";
 
 import { fetchFiles } from "redux/files/action";
 import FileModal from "../components/FileModal";
 import DataPage from "components/DataPage";
+import { Download } from "mdi-material-ui";
 
 const styles = (theme) => ({
     centerItem: theme.styles.centerItem,
@@ -59,7 +60,26 @@ class Assignments extends React.Component {
             headerName: "Description",
             headerAlign: "center",
             align: "center",
-            flex: 2.5,
+            flex: 2,
+        },
+        {
+            field: "actions",
+            headerName: "Actions",
+            headerAlign: "center",
+            flex: 1,
+            sortable: false,
+            filterable: false,
+            renderCell: (params) => (
+                <div className={this.props.classes.centerItem}>
+                    <Tooltip title="Download">
+                        <a href={params.row.url} target="_blank" rel="noreferrer noopener">
+                            <IconButton color={"primary"}>
+                                <Download />
+                            </IconButton>
+                        </a>
+                    </Tooltip>
+                </div>
+            ),
         },
     ];
 
